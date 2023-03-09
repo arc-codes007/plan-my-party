f8608a9280378dc20be34850ced176748e4cbef4
Utkarsh Bhati <utkarshbhati2000@gmail.com>
2023-03-09 19:05:49 +0530

    Made changes added button in venue card

--- resources/views/components/venue_card.blade.php
+++ resources/views/components/venue_card.blade.php
@@ -41,6 +41,9 @@
                             </div>
                         @endif
                     </div>
+                    <div class="d-grid gap-2 mt-2">
+                        <button role="button" class="btn btn-sm btn-danger text-white">Book Now!</button>
+                    </div>
                 </div>
             </div>
         </div>
