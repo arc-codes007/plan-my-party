182499c6c6b2dc79f4fb2254eaee4917adc9976e
Yash Mathur <yashmathur9274@gmail.com>
2023-03-10 19:51:55 +0530

    footer added as component corousel fixed in landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,35 +19,6 @@
     <hr>
 </div>
 
-<section id="footer" class="pt-4 pb-2 border-bottom">
-    <div class="container">
-        <div class="d-flex justify-content-around">
-            <div>
-                <h4>About</h4>
-                <a href="#" class="text-black">Home</a><br>
-                <a href="{{ route('about') }}" class="text-black">About Us</a><br>
-                <a href="mailto:info@pmp.in" class="text-primary text-semibold" style="text-decoration: none"><i class="fa-regular fa-envelope"></i> Write Us</a><br>
-                <a href="tel:+9876543210" class="text-primary text-semibold" style="text-decoration: none"><i class="fa-solid fa-phone"></i> Phone</a><br>
-                <!-- <a href="#" class="text-black">Contact US</a> -->
-            </div>
-            <div>
-                <h4>Quick Links</h4>
-                <a href="#venue" class="text-black">Venues</a><br>
-                <a href="#package" class="text-black">Packages</a><br>
-                <a href="{{ route('login') }}" class="text-black">Login</a><br>
-                <a href="{{ route('register') }}" class="text-black">Register</a>
-            </div>
-            <div>
-                <h4>Socials</h4>
-                <a href="#" class="text-primary fw-bold" style="text-decoration: none"><i class="fa-brands fa-facebook-f fa-lg"></i> Facebook</a><br>
-                <a href="#" class="text-danger fw-bold" style="text-decoration: none"><i class="fa-brands fa-instagram fa-lg"></i> Instagram</a><br>
-                <a href="#" class="text-primary fw-bold" style="text-decoration: none"><i class="fa-brands fa-linkedin-in fa-lg"></i> Linked In</a><br>
-            </div>
-        </div>
-
-        <hr>
-        <p class="text-center">Plan-my-Party | Designed and Developed by Utkarsh, Ritik, Yash, Pratik and Vaibhav</p>
-    </div>
-</section>
+@include('components.footer')
 
 @endsection
